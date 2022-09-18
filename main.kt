5110be65153e46c8d23e4dd5698d57935644c292
Jack Tolmie <jacktolmie@hotmail.com>
2022-09-18 11:18:43 -0700

    Feat: Added conditional checks for character input. Added Colours.BLANK to add as an element when checking the colours selected against the code. Fixed issue with checking code messing up the pegging section if a peg matched colour but not the spot.

--- main.kt
+++ main.kt
@@ -1,15 +1,19 @@
 enum class Colours{
-    BLUE, GREEN, YELLOW, RED, PURPLE, ORANGE, BLANK
+    BLUE, GREEN, YELLOW, RED, PURPLE, ORANGE, EMPTY, BLANK
 }
 
 //Ask the player to choose the colours wanted to test.
 fun getColours(): MutableList<Colours>{
     val chosenColour = mutableListOf<Colours>()
 
-    println("Colours to choose from: (B)lue, (G)reen, (Y)ellow, (R)ed, (P)urple, (O)range")
+    println("Choose from: (B)lue, (G)reen, (Y)ellow, (R)ed, (P)urple, (O)range, (E)mpty")
     while(chosenColour.size < 4){
         print("Please enter colour for #${chosenColour.size +1}: ")
         var choice = readln().uppercase()
+
+        /* If the choice is single char, change
+        *  it to the full name of colour.
+        */
         if(choice.length == 1){
             when(choice){
                 "B" -> choice = "BLUE"
@@ -18,8 +22,13 @@ fun getColours(): MutableList<Colours>{
                 "R" -> choice = "RED"
                 "P" -> choice = "PURPLE"
                 "O" -> choice = "ORANGE"
+                "E" -> choice = "EMPTY"
             }
         }
+        /* If the Colours enum contains the colour selected
+        *  add it to the chosenColour list, otherwise
+        *  print warning.
+        */
         if(Colours.values().map { it.name}.contains(choice)) {
             when (choice) {
                 "BLUE"-> chosenColour.add(Colours.BLUE)
@@ -28,6 +37,7 @@ fun getColours(): MutableList<Colours>{
                 "RED" -> chosenColour.add(Colours.RED)
                 "PURPLE" -> chosenColour.add(Colours.PURPLE)
                 "ORANGE" -> chosenColour.add(Colours.ORANGE)
+                "EMPTY" ->  chosenColour.add(Colours.EMPTY)
             }
         }
         else println("$choice is not an option. Please choose again.")
@@ -36,53 +46,99 @@ fun getColours(): MutableList<Colours>{
 }
 
 //Make the code to guess.
-fun codeMaker(): MutableList<Colours>{
+fun codeMaker(): List<Colours>{
     val code = mutableListOf<Colours>()
+
+    /* Choose four random colours from the
+    *  enum class. If BLANK is selected do
+    *  not add it to the list.
+    */
     while(code.size < 4){
         val colour = Colours.values().random()
         if(colour != Colours.BLANK){
             code.add(colour)
         }
     }
-    code.forEach { print("$it, ") }
-    println()
-    return code
+    return code.toList()
 }
 
-fun checkCode(code : MutableList<Colours>, guess : MutableList<Colours>): MutableList<Char>{
+fun checkCode(code : List<Colours>, guess : MutableList<Colours>): MutableList<Char>{
 
     val peg = mutableListOf<Char>()
+    val copyCode = code.toMutableList()
 
-    if(code == guess){
-        println("Congratulations. You won!")
+    /* If all the choices are right, and in the right order,
+    *  fill list with 'b' and return.
+    */
+    if(copyCode == guess){
+        (1..4).forEach{ _ ->
+            peg.add('b')
+        }
+        return peg
     }
     else{
-        println("Incorrect choices!")
+        for(i in 0..3) {
 
-        for(i in 0..3){
-            if(guess[i] == code[i]){
+            /* Check if current guess matches the code. Add 'b' if it does and
+            *  set the code element to BLANK.
+            */
+            if (guess[i] == copyCode[i]) {
                 peg.add('b')
-                code[i] = Colours.BLANK
-            }
-            else if(code.contains(guess[i])){
-                peg.add('w')
-                code[i] = Colours.BLANK
+                copyCode[i] = Colours.BLANK
+                continue
             }
         }
+
+
+      for(i in 0..3) {
+
+          // Count how many times current guess is in the list.
+          val colourCount = copyCode.count {guess[i].name == it.name}
+
+          /* If the count is only one, then it will not match
+          *  any other part of the code. Add 'w' and set to BLANK.
+          */
+          if(colourCount > 0){
+              peg.add('w')
+              copyCode[copyCode.indexOf(guess[i])] = Colours.BLANK
+              continue
+          }
+      }
     }
-//    peg.forEach{print("$it, ")}
     peg.shuffle()
     return peg
 }
 
-//fun pegs(peg : List<Char>){
-//
-//
-//}
+fun printRow(choices : MutableList<Map<MutableList<Colours>, MutableList<Char>>>){
+    choices.forEachIndexed {i, choice ->
+        choice.forEach { (t, u) ->
+            print("${i + 1}\t")
+            t.forEach { print("$it ") }
+            print(" : ")
+            u.forEach { print("$it ") }
+            println()
+        }
+    }
+
+}
 fun main(){
     val code = codeMaker()
-    val choseColour = getColours()
-    checkCode(code, choseColour)
+    val storeGuesses = mutableListOf<Map<MutableList<Colours>, MutableList<Char>>>()
+
+    for(tries in 1..12){
+        val chooseColour = getColours()
+        val checkChoices = checkCode(code, chooseColour)
+        storeGuesses.add(mapOf(chooseColour to checkChoices))
+        printRow(storeGuesses)
+        if((checkChoices.all { it == 'b'})  && checkChoices.size == 4){
+            println("Congratulations. You won!")
+            break
+        }
+    }
+
+    print("The code was: ")
+    code.forEach { print("$it ") }
+    println()
 
 
 
